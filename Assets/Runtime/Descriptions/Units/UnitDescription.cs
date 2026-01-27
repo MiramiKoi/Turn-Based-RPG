30d9b547aaed9f4a167c5240a37b0d8c792def02
DmitryDemenkov <demon.krut2002@mail.ru>
2026-01-27 18:02:02 +0300

    add: unit description class

---
+++ Assets/Runtime/Descriptions/Units/UnitDescription.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Runtime.Descriptions.Stats;
+using Runtime.Extensions;
+
+namespace Runtime.Descriptions.Units
+{
+    public class UnitDescription : Description
+    {
+        public StatDescriptionCollection Stats { get; }
+        
+        public UnitDescription(string id, Dictionary<string, object> data) : base(id)
+        {
+            Stats = new StatDescriptionCollection(data.GetNode("stats"));
+        }
+    }
+}
