3732da0d6b53c43832cb112395a69d2ca18a4a44
DmitryDemenkov <demon.krut2002@mail.ru>
2026-01-27 17:58:42 +0300

    add: description class

---
+++ Assets/Runtime/Descriptions/Description.cs
@@ -0,0 +1,11 @@
+namespace Runtime.Descriptions
+{
+    public abstract class Description
+    {
+        public string Id { get; }
+        protected Description(string id)
+        {
+            Id = id;
+        }
+    }
+}
