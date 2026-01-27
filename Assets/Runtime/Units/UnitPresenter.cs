8fe540febbb8f954bc77715308a56ccfe13dc008
DmitryDemenkov <demon.krut2002@mail.ru>
2026-01-27 18:06:04 +0300

    add: unit presenter

---
+++ Assets/Runtime/Units/UnitPresenter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Runtime.Common;
+using UniRx;
+using UnityEngine;
+
+namespace Runtime.Units
+{
+    public class UnitPresenter : IPresenter
+    {
+        private readonly List<IDisposable> _disposables = new();
+        
+        private readonly UnitModel _unit;
+        private readonly UnitView _view;
+
+        public UnitPresenter(UnitModel unit, UnitView view)
+        {
+            _unit = unit;
+            _view = view;
+        }
+        
+        public void Enable()
+        {
+            _unit.Position.Subscribe(OnPositionChanged).AddTo(_disposables);
+        }
+
+        public void Disable()
+        {
+            _disposables.ForEach(x => x.Dispose());
+            _disposables.Clear();
+        }
+        
+        private void OnPositionChanged(Vector2Int position)
+        {
+            _view.Transform.position = new Vector3(position.x, -position.y, 0);
+        }
+    }
+}
