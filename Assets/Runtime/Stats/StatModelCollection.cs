713a8e81bb89b6f47b7782c2ff408bfd110e625e
DmitryDemenkov <demon.krut2002@mail.ru>
2026-01-27 18:00:41 +0300

    add: stats model

---
+++ Assets/Runtime/Stats/StatModelCollection.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Runtime.Descriptions.Stats;
+
+namespace Runtime.Stats
+{
+    public class StatModelCollection : IEnumerable<StatModel>
+    {
+        public Dictionary<string, StatModel> Stats { get; }
+        
+        public StatModelCollection(StatDescriptionCollection descriptionCollection)
+        {
+            Stats = new Dictionary<string, StatModel>();
+
+            foreach (var description in descriptionCollection)
+            {
+                Stats.Add(description.Id, new StatModel(description));
+            }
+        }
+
+        public IEnumerator<StatModel> GetEnumerator()
+        {
+            return Stats.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int this[string id] => (int)Stats[id].Value;
+    }
+}
