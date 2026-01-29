97b7ec09e073404bcfb808760fff56d725802270
Valyutik <sergei.lind@tutamail.com>
2026-01-29 16:31:16 +0300

    chg: use input system for player attack action

--- Assets/Runtime/Landscape/Grid/Interaction/GridInteractionModel.cs
+++ Assets/Runtime/Landscape/Grid/Interaction/GridInteractionModel.cs
@@ -7,18 +7,12 @@ namespace Runtime.Landscape.Grid.Interaction
     {
         public CellModel CurrentCell { get; private set; }
         public event Action<CellModel> OnCellChanged;
-        public event Action<CellModel> OnCellSelected;
         
         public void SetCell(CellModel cell)
         {
             CurrentCell = cell;
             
             OnCellChanged?.Invoke(cell);
-        }
-        
-        public void SelectCell()
-        {
-            OnCellSelected?.Invoke(CurrentCell);
         }
     }
 }
--- Assets/Runtime/Player/PlayerPresenter.cs
+++ Assets/Runtime/Player/PlayerPresenter.cs
@@ -1,15 +1,15 @@
 using System;
 using Runtime.Common;
-using Runtime.Landscape.Grid.Cell;
 using Runtime.Units;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Runtime.Player
 {
     public class PlayerPresenter : IPresenter
     {
-        private UnitModel _character;
-        private World _world;
+        private readonly UnitModel _character;
+        private readonly World _world;
 
         public PlayerPresenter(UnitModel character, World world)
         {
@@ -19,41 +19,46 @@ namespace Runtime.Player
 
         public void Enable()
         {
-            _world.GridInteractionModel.OnCellSelected += OnCellSelected;
+            _world.PlayerControls.Gameplay.Attack.performed += OnCellSelected;
         }
 
         public void Disable()
         {
-            _world.GridInteractionModel.OnCellSelected -= OnCellSelected;
+            _world.PlayerControls.Gameplay.Attack.performed -= OnCellSelected;
         }
 
-        private void OnCellSelected(CellModel selectedCell)
+        private void OnCellSelected(InputAction.CallbackContext context)
         {
-            var newCellPositionX = selectedCell.Position.x - _character.Position.Value.x;
-            var newCellPositionY = selectedCell.Position.y - _character.Position.Value.y;
-            
-            newCellPositionX = Math.Clamp(newCellPositionX, -1, 1);
-            newCellPositionY = Math.Clamp(newCellPositionY, -1, 1);
-            
-            newCellPositionX += _character.Position.Value.x;
-            newCellPositionY += _character.Position.Value.y;
-            
-            var newCellPosition = new Vector2Int(newCellPositionX, newCellPositionY);
-            
-            if (_world.GridModel.CanPlace(newCellPosition))
+            if (_world.GridInteractionModel.CurrentCell != null)
             {
-                if (newCellPosition.x != _character.Position.Value.x)
-                {
-                    var newDirection = newCellPosition.x < _character.Position.Value.x
-                        ? UnitDirection.Left
-                        : UnitDirection.Right;
-                    _character.Rotate(newDirection);
-                }
+                var selectedCell = _world.GridInteractionModel.CurrentCell;
 
-                _world.GridModel.ReleaseCell(_character.Position.Value);
-                _world.GridModel.TryPlace(_character, newCellPosition);
-                
-                _character.MoveTo(newCellPosition);
+                var newCellPositionX = selectedCell.Position.x - _character.Position.Value.x;
+                var newCellPositionY = selectedCell.Position.y - _character.Position.Value.y;
+
+                newCellPositionX = Math.Clamp(newCellPositionX, -1, 1);
+                newCellPositionY = Math.Clamp(newCellPositionY, -1, 1);
+
+                newCellPositionX += _character.Position.Value.x;
+                newCellPositionY += _character.Position.Value.y;
+
+                var newCellPosition = new Vector2Int(newCellPositionX, newCellPositionY);
+
+                if (_world.GridModel.CanPlace(newCellPosition))
+                {
+                    if (newCellPosition.x != _character.Position.Value.x)
+                    {
+                        var newDirection = newCellPosition.x < _character.Position.Value.x
+                            ? UnitDirection.Left
+                            : UnitDirection.Right;
+                        _character.Rotate(newDirection);
+                    }
+
+                    _world.GridModel.ReleaseCell(_character.Position.Value);
+                    _world.GridModel.TryPlace(_character, newCellPosition);
+
+                    _character.MoveTo(newCellPosition);
+                }
             }
         }
     }
