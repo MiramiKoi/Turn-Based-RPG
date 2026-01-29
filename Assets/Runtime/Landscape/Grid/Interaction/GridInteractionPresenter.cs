48386496509e24ebf8fe4dde8c1b7fe25ef72d67
Valyutik <sergei.lind@tutamail.com>
2026-01-29 23:45:44 +0300

    refactor: extract grid interaction to system

--- Assets/Runtime/Landscape/Grid/Interaction/GridInteractionPresenter.cs
+++ Assets/Runtime/Landscape/Grid/Interaction/GridInteractionPresenter.cs
@@ -1,62 +1,31 @@
 using Runtime.Common;
 using Runtime.Landscape.Grid.Indication;
-using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Runtime.Landscape.Grid.Interaction
 {
     public class GridInteractionPresenter : IPresenter
     {
-        private readonly GridView _view;
         private readonly GridInteractionModel _model;
         private readonly World _world;
+        
+        private readonly GridInteractionSystem _system;
 
         public GridInteractionPresenter(GridInteractionModel model, GridView view, World world)
         {
             _model = model;
-            _view = view;
             _world = world;
+            _system = new GridInteractionSystem(model, view, world);                
         }
 
         public void Enable()
         {
-            _world.PlayerControls.Gameplay.PointerMove.performed += OnPointerMove;
+            _world.GameSystems.Add(_system);
         }
 
         public void Disable()
         {
-            _world.PlayerControls.Gameplay.PointerMove.performed -= OnPointerMove;
             Clear();
-        }
-
-        private void OnPointerMove(InputAction.CallbackContext context)
-        {
-            if (_model.IsActive)
-            {
-                var mousePosition = context.ReadValue<Vector2>();
-
-                var worldPosition =
-                    _world.MainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0));
-                var nextCellPosition = _view.Tilemap.WorldToCell(worldPosition);
-                if (nextCellPosition is { x: < GridConstants.Width, y: < GridConstants.Height } and
-                    { x: >= 0, y: >= 0 })
-                {
-                    var cell = _world.GridModel.GetCell(new Vector2Int(nextCellPosition.x, nextCellPosition.y));
-
-                    if (cell != _model.CurrentCell)
-                    {
-                        if (_model.CurrentCell != null)
-                        {
-                            _world.GridModel.Cells[_model.CurrentCell.Position.x, _model.CurrentCell.Position.y]
-                                .SetIndication(IndicationType.Null);
-                        }
-
-                        _model.SetCell(cell);
-                        _world.GridModel.Cells[nextCellPosition.x, nextCellPosition.y]
-                            .SetIndication(IndicationType.Cursor);
-                    }
-                }
-            }
+            _world.GameSystems.Remove(_system);
         }
 
         private void Clear()
---
+++ Assets/Runtime/Landscape/Grid/Interaction/GridInteractionSystem.cs
@@ -0,0 +1,52 @@
+﻿using Runtime.GameSystems;
+using Runtime.Landscape.Grid.Indication;
+using UnityEngine;
+
+namespace Runtime.Landscape.Grid.Interaction
+{
+    public class GridInteractionSystem : IGameSystem
+    {
+        public string Id => "grid-interaction-system";
+        
+        private readonly GridInteractionModel _model;
+        private readonly GridView _view;
+        private readonly World _world;
+
+        public GridInteractionSystem(GridInteractionModel model, GridView view, World world)
+        {
+            _model = model;
+            _view = view;
+            _world = world;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (_model.IsActive)
+            {
+                var mousePosition = _world.PlayerControls.Gameplay.PointerMove.ReadValue<Vector2>();
+
+                var worldPosition =
+                    _world.MainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0));
+                var nextCellPosition = _view.Tilemap.WorldToCell(worldPosition);
+                if (nextCellPosition is { x: < GridConstants.Width, y: < GridConstants.Height } and
+                    { x: >= 0, y: >= 0 })
+                {
+                    var cell = _world.GridModel.GetCell(new Vector2Int(nextCellPosition.x, nextCellPosition.y));
+
+                    if (cell != _model.CurrentCell)
+                    {
+                        if (_model.CurrentCell != null)
+                        {
+                            _world.GridModel.Cells[_model.CurrentCell.Position.x, _model.CurrentCell.Position.y]
+                                .SetIndication(IndicationType.Null);
+                        }
+
+                        _model.SetCell(cell);
+                        _world.GridModel.Cells[nextCellPosition.x, nextCellPosition.y]
+                            .SetIndication(IndicationType.Cursor);
+                    }
+                }
+            }
+        }
+    }
+}
