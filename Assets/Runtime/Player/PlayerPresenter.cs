f40e59b60b53b13316d82e62c381ef39c8f0337b
Valyutik <sergei.lind@tutamail.com>
2026-01-29 22:33:42 +0300

    add: attack input to execute movement route

--- Assets/Runtime/Player/PlayerPresenter.cs
+++ Assets/Runtime/Player/PlayerPresenter.cs
@@ -4,6 +4,7 @@ using Runtime.Common.Movement;
 using Runtime.Landscape.Grid.Indication;
 using Runtime.Units;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Runtime.Player
 {
@@ -12,6 +13,8 @@ namespace Runtime.Player
         private readonly UnitModel _model;
         private readonly World _world;
         private readonly MovementQueueModel _movementQueueModel;
+
+        private bool _isExecutingRoute;
 
         public PlayerPresenter(UnitModel model, World world)
         {
@@ -23,68 +26,110 @@ namespace Runtime.Player
         public void Enable()
         {
             _world.GridInteractionModel.OnCurrentCellChanged += HandleInteractionCellChanged;
-        } 
+            _world.PlayerControls.Gameplay.Attack.performed += HandleAttackPerformed;
+            _world.TurnBaseModel.OnStepFinished += HandleTurnFinished;
+        }
 
         public void Disable()
         {
             _world.GridInteractionModel.OnCurrentCellChanged -= HandleInteractionCellChanged;
+            _world.PlayerControls.Gameplay.Attack.performed -= HandleAttackPerformed;
+            _world.TurnBaseModel.OnStepFinished -= HandleTurnFinished;
+        }
+
+        private void ExecuteNextStep()
+        {
+            if (_isExecutingRoute)
+            {
+                if (_movementQueueModel.HasSteps)
+                {
+                    var nextCell = _movementQueueModel.Dequeue();
+
+                    if (_world.GridModel.CanPlace(nextCell))
+                    {
+                        RotateCharacter(nextCell);
+
+                        _world.GridModel.ReleaseCell(_model.Position.Value);
+                        _world.GridModel.TryPlace(_model, nextCell);
+                        _model.MoveTo(nextCell);
+
+                        _world.TurnBaseModel.Step();
+                    }
+                    else
+                    {
+                        StopRoute();
+                    }
+                }
+                else
+                {
+                    StopRoute();
+                }
+            }
+        }
+
+        private void RotateCharacter(Vector2Int next)
+        {
+            var current = _model.Position.Value;
+            if (next.x != current.x)
+                _model.Rotate(next.x < current.x ? UnitDirection.Left : UnitDirection.Right);
+        }
+
+        private void StopRoute()
+        {
+            _isExecutingRoute = false;
+            _movementQueueModel.Clear();
+            ClearRouteIndication();
+        }
+
+        private void ClearRouteIndication()
+        {
+            foreach (var position in _movementQueueModel.Steps)
+            {
+                _world.GridModel.Cells[position.x, position.y].SetIndication(IndicationType.Null);
+            }
+        }
+
+        private void HandleAttackPerformed(InputAction.CallbackContext obj)
+        {
+            if (_movementQueueModel.HasSteps)
+            {
+                _isExecutingRoute = true;
+                ExecuteNextStep();
+            }
+        }
+
+        private void HandleTurnFinished()
+        {
+            if (_isExecutingRoute)
+                ExecuteNextStep();
         }
 
         private void HandleInteractionCellChanged()
         {
-            if (_world.GridInteractionModel.CurrentCell == null)
-                return;
-
-            var start = _model.Position.Value;
-            var target = _world.GridInteractionModel.CurrentCell.Position;
-
-            if (_movementQueueModel.HasSteps)
+            if (!_isExecutingRoute)
             {
-                foreach (var position in _movementQueueModel.Steps)
-                    _world.GridModel.Cells[position.x, position.y].SetIndication(IndicationType.Null);
+                if (_world.GridInteractionModel.CurrentCell != null)
+                {
+                    var start = _model.Position.Value;
+                    var target = _world.GridInteractionModel.CurrentCell.Position;
+
+                    if (_movementQueueModel.HasSteps)
+                    {
+                        foreach (var position in _movementQueueModel.Steps)
+                            _world.GridModel.Cells[position.x, position.y].SetIndication(IndicationType.Null);
+                    }
+
+                    var path = GridPathfinder.FindPath(
+                        _world.GridModel,
+                        start,
+                        target);
+
+                    _movementQueueModel.SetPath(path);
+
+                    foreach (var position in path.Where(position => _model.Position.Value != position))
+                        _world.GridModel.Cells[position.x, position.y].SetIndication(IndicationType.RoutePoint);
+                }
             }
-
-            var path = GridPathfinder.FindPath(
-                _world.GridModel,
-                start,
-                target);
-
-            _movementQueueModel.SetPath(path);
-            
-            foreach (var position in path.Where(position => _model.Position.Value != position))
-                _world.GridModel.Cells[position.x, position.y].SetIndication(IndicationType.RoutePoint);
-        }
-
-        private void MoveCharacter()
-        {
-            if (!_movementQueueModel.HasSteps)
-                return;
-
-            var nextCell = _movementQueueModel.Dequeue();
-
-            if (!_world.GridModel.CanPlace(nextCell))
-            {
-                _movementQueueModel.Clear();
-                return;
-            }
-
-            RotateCharacter(nextCell);
-
-            _world.GridModel.ReleaseCell(_model.Position.Value);
-            _world.GridModel.TryPlace(_model, nextCell);
-            _model.MoveTo(nextCell);
-        }
-        
-        private void RotateCharacter(Vector2Int next)
-        {
-            var current = _model.Position.Value;
-            if (next.x == current.x)
-                return;
-
-            _model.Rotate(
-                next.x < current.x
-                    ? UnitDirection.Left
-                    : UnitDirection.Right);
         }
     }
 }
