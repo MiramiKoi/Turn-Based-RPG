a7a0726949e2d232f7747db41422f2d9123c4e7b
Valyutik <sergei.lind@tutamail.com>
2026-01-29 20:26:58 +0300

    add: cell change event to grid interaction

--- Assets/Runtime/Landscape/Grid/Interaction/GridInteractionModel.cs
+++ Assets/Runtime/Landscape/Grid/Interaction/GridInteractionModel.cs
@@ -1,14 +1,18 @@
+using System;
 using Runtime.Landscape.Grid.Cell;
 
 namespace Runtime.Landscape.Grid.Interaction
 {
     public class GridInteractionModel
     {
+        public event Action OnCurrentCellChanged;
+        
         public CellModel CurrentCell { get; private set; }
 
         public void SetCell(CellModel cell)
         {
             CurrentCell = cell;
+            OnCurrentCellChanged?.Invoke();
         }
     }
 }
