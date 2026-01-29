4e33af0f52e946321e327d4b92dd8b85696eb118
Valyutik <sergei.lind@tutamail.com>
2026-01-29 19:37:19 +0300

    add: route point tile asset and indicator

--- Assets/Runtime/ViewDescriptions/Landscape/Grid/GridIndicationViewDescription.cs
+++ Assets/Runtime/ViewDescriptions/Landscape/Grid/GridIndicationViewDescription.cs
@@ -7,6 +7,7 @@ namespace Runtime.ViewDescriptions.Landscape.Grid
     [CreateAssetMenu(fileName = "GridIndicationViewDescription", menuName = "ViewDescription/GridIndicationViewDescription")]
     public class GridIndicationViewDescription : ScriptableObject
     {
-        public AssetReferenceT<Tile> TileAsset;
+        public AssetReferenceT<Tile> CellCursorAsset;
+        public AssetReferenceT<Tile> RoutePointAsset;
     }
 }
