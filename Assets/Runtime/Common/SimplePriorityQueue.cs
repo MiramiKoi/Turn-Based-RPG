b99b75a8363afc80fc568d5fe61d9512d72ca398
Valyutik <sergei.lind@tutamail.com>
2026-01-29 17:45:11 +0300

    add: simple priority queue generic class

---
+++ Assets/Runtime/Common/SimplePriorityQueue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Runtime.Common
+{
+    public class SimplePriorityQueue<T>
+    {
+        private readonly List<(T item, int priority)> _items = new();
+
+        public int Count => _items.Count;
+
+        public void Enqueue(T item, int priority)
+        {
+            _items.Add((item, priority));
+        }
+
+        public T Dequeue()
+        {
+            var bestIndex = 0;
+
+            for (var i = 1; i < _items.Count; i++)
+            {
+                if (_items[i].priority < _items[bestIndex].priority)
+                    bestIndex = i;
+            }
+
+            var bestItem = _items[bestIndex].item;
+            _items.RemoveAt(bestIndex);
+            return bestItem;
+        }
+    }
+}
