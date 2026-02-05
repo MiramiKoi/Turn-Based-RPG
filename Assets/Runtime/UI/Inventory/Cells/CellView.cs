e051b94a1c4e7d27deabe2b11d23d3b9f3c5b6cf
Alexei <153764172+MiramiKoi@users.noreply.github.com>
2026-02-05 17:45:01 +0300

    chg: added amount to cell view

--- Assets/Runtime/UI/Inventory/Cells/CellView.cs
+++ Assets/Runtime/UI/Inventory/Cells/CellView.cs
@@ -6,11 +6,13 @@ namespace Runtime.UI.Inventory.Cells
     {
         public VisualElement Root { get; }
         public VisualElement Icon { get; }
+        public Label Amount { get; }
 
         public CellView(VisualTreeAsset asset)
         {
             Root = asset.CloneTree().Q<VisualElement>("cell");
             Icon = Root.Q<VisualElement>("icon");
+            Amount = Root.Q<Label>("amount");
         }
     }
 }
