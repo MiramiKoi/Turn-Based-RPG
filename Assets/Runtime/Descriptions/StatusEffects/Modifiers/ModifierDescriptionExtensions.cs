91273795973bb8f0a1bd6a20ad3865a98e2dfcbd
Valyutik <sergei.lind@tutamail.com>
2026-02-06 18:00:03 +0300

    add: introduce disable actions modifier description and extend modifier conversion logic

--- Assets/Runtime/Descriptions/StatusEffects/Modifiers/ModifierDescriptionExtensions.cs
+++ Assets/Runtime/Descriptions/StatusEffects/Modifiers/ModifierDescriptionExtensions.cs
@@ -5,12 +5,14 @@ namespace Runtime.Descriptions.StatusEffects.Modifiers
     public static class ModifierDescriptionExtensions
     {
         private const string ChangeStatKey = "change_stat";
+        private const string DisableActionsKey = "disable_actions";
         
         public static ModifierDescription ToModifierDescription(this Dictionary<string, object> data, string type)
         {
             return type switch
             {
                 ChangeStatKey => new ChangeStatModifierDescription(data),
+                DisableActionsKey => new DisableActionsModifierDescription(data),
                 _ =>  null
             };
         }
