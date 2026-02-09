7762fa6b51c6a5b6a310412b3b46b3ba26ec0aa8
Valyutik <sergei.lind@tutamail.com>
2026-02-09 21:31:34 +0300

    refactor: reorganize and simplify status effect descriptions structure

--- Assets/Runtime/Descriptions/StatusEffects/StatusEffectDescription.cs
+++ Assets/Runtime/Descriptions/StatusEffects/StatusEffectDescription.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using Runtime.Descriptions.StatusEffects.Constraints;
 using Runtime.Descriptions.StatusEffects.Enums;
-using Runtime.Descriptions.StatusEffects.Modifiers;
 using Runtime.Extensions;
 
 namespace Runtime.Descriptions.StatusEffects
@@ -12,19 +10,15 @@ namespace Runtime.Descriptions.StatusEffects
         private const string PolarityKey = "polarity";
         private const string StackingKey = "stacking";
         private const string DurationKey = "duration";
-        private const string ModifiersKey = "modifiers";
-        private const string ConstraintsKey = "constraints";
         private const string FlagsKey = "flags";
 
         public string ViewId { get; }
         public StatusCategory Category { get; }
         public Polarity Polarity { get; }
+        public string LuaScript { get; set; }
 
         public StackingDescription Stacking { get; }
         public DurationDescription Duration { get; }
-
-        public List<ModifierDescription> Modifiers { get; } = new();
-        public List<ConstraintDescription> Constraint { get; } = new();
 
         public StatusFlag Flags { get; }
 
@@ -35,26 +29,10 @@ namespace Runtime.Descriptions.StatusEffects
             Category = data.GetEnum<StatusCategory>(CategoryKey);
             Polarity = data.GetEnum<Polarity>(PolarityKey);
 
+            LuaScript = data.GetString("lua_script");
+
             Stacking = new StackingDescription(data.GetNode(StackingKey));
             Duration = new DurationDescription(data.GetNode(DurationKey));
-
-            var modifierData = data.GetNode(ModifiersKey);
-
-            foreach (var modifier in modifierData)
-            {
-                var modifierDictionary = (Dictionary<string, object>)modifier.Value;
-
-                Modifiers.Add(modifierDictionary.ToModifierDescription(modifier.Key));
-            }
-
-            var constraintData = data.GetNode(ConstraintsKey);
-
-            foreach (var constraint in constraintData)
-            {
-                var constraintDictionary = (Dictionary<string, object>)constraint.Value;
-
-                Constraint.Add(constraintDictionary.ToConstraintDescription(constraint.Key));
-            }
 
             Flags = data.GetFlags<StatusFlag>(FlagsKey);
         }
