ff5ce751687e9a411fabc25e87fca57487683750
Valyutik <sergei.lind@tutamail.com>
2026-02-10 01:11:51 +0300

    add: status effect applier

---
+++ Assets/Runtime/StatusEffects/Applier/StatusEffectApplierModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Runtime.Descriptions;
+using Runtime.StatusEffects.Collection;
+
+namespace Runtime.StatusEffects.Applier
+{
+    public class StatusEffectApplierModel
+    {
+        public event Action<string> OnApplyRequested;
+        public event Action<string> OnRemoveRequested;
+
+        public StatusEffectModelCollection Collection { get; }
+
+        public StatusEffectApplierModel(WorldDescription description)
+        {
+            Collection = new StatusEffectModelCollection(description.StatusEffectCollection);
+        }
+
+        public void TryApply(string descriptionKey)
+        {
+            OnApplyRequested?.Invoke(descriptionKey);
+        }
+
+        public void TryRemove(string descriptionKey)
+        {
+            OnRemoveRequested?.Invoke(descriptionKey);
+        }
+    }
+}
---
+++ Assets/Runtime/StatusEffects/Applier/StatusEffectApplierPresenter.cs
@@ -0,0 +1,77 @@
+﻿using MoonSharp.Interpreter;
+using Runtime.Common;
+using Runtime.Core;
+using Runtime.Descriptions.StatusEffects;
+using Runtime.Units;
+using UnityEngine;
+
+namespace Runtime.StatusEffects.Applier
+{
+    public class StatusEffectApplierPresenter : IPresenter
+    {
+        private readonly StatusEffectApplierModel _model;
+        private readonly UnitModel _unit;
+        private readonly World _world;
+
+        public StatusEffectApplierPresenter(StatusEffectApplierModel model, UnitModel unit, World world)
+        {
+            _model = model;
+            _unit = unit;
+            _world = world;
+        }
+
+        public void Enable()
+        {
+            _model.OnApplyRequested += HandleApplyRequested;
+            _model.OnRemoveRequested += HandleRemoveRequested;
+        }
+
+        public void Disable()
+        {
+            _model.OnApplyRequested -= HandleApplyRequested;
+            _model.OnRemoveRequested -= HandleRemoveRequested;
+        }
+
+        private bool CanApply(StatusEffectDescription description)
+        {
+            var module = LuaRuntime.Instance.GetModuleAsync(description.LuaScript);
+
+            var function = module.Get("CanApply");
+
+            if (!function.IsNil())
+            {
+                var context = new Table(LuaRuntime.Instance.LuaScript);
+                var effectTable = new Table(LuaRuntime.Instance.LuaScript)
+                {
+                    ["stacks"] = 1,
+                    ["remaining_turns"] = description.Duration.Turns
+                };
+
+                context["unit"] = UserData.Create(_unit);
+                context["world"] = UserData.Create(_world);
+                context["effect"] = effectTable;
+
+                var result = LuaRuntime.Instance.LuaScript.Call(function, context);
+                return result.CastToBool();
+            }
+
+            return true;
+        }
+        
+        private void HandleApplyRequested(string effectId)
+        {
+            var description = _world.WorldDescription.StatusEffectCollection.Get(effectId);
+
+            if (CanApply(description))
+            {
+                _model.Collection.Create(effectId);
+            }
+            Debug.Log(CanApply(description));
+        }
+
+        private void HandleRemoveRequested(string effectId)
+        {
+            _model.Collection.Remove(effectId);
+        }
+    }
+}
