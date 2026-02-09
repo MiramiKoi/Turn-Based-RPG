045ebd7e04166005dd9910fc8b8779267bfeca05
Valyutik <sergei.lind@tutamail.com>
2026-02-09 21:31:50 +0300

    add: implement base structure for status effect

---
+++ Assets/Content/Descriptions/StatusEffects/burn.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+function M.CanApply(ctx)
+    
+    return true
+end 
+
+function M.OnApply(ctx)
+    
+end
+
+function M.OnTick(ctx)
+   
+end
+
+function M.OnRemove(ctx)
+    
+end
+
+return M
