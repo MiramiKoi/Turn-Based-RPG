0fc8bdd5ecfccea3e2df562fcecfc4f838ac9658
Alexei <153764172+MiramiKoi@users.noreply.github.com>
2026-02-10 21:38:10 +0300

    add: loot presenter

---
+++ Assets/Runtime/UI/Loot/LootPresenter.cs
@@ -0,0 +1,61 @@
+using Runtime.Common;
+using Runtime.Core;
+using Runtime.UI.Inventory;
+using Runtime.Units;
+using Runtime.ViewDescriptions;
+
+namespace Runtime.UI.Loot
+{
+    public class LootPresenter : IPresenter
+    {
+        private InventoryPresenter _currentInventory;
+
+        private readonly World _world;
+        private readonly UIContent _uiContent;
+        private readonly InventoryView _inventoryView;
+        private readonly WorldViewDescriptions _viewDescriptions;
+
+        public LootPresenter(World world, UIContent uiContent, WorldViewDescriptions viewDescriptions)
+        {
+            _world = world;
+            _uiContent = uiContent;
+            _viewDescriptions = viewDescriptions;
+
+            _inventoryView = new InventoryView(_viewDescriptions.InventoryViewDescription.InventoryAsset);
+            _inventoryView.Root.AddToClassList("loot-inventory");
+        }
+
+        public void Enable()
+        {
+            _world.LootModel.OnLootRequested += Show;
+            _world.TurnBaseModel.OnPlayerStepFinished += Clear;
+        }
+
+        public void Disable()
+        {
+            _world.LootModel.OnLootRequested -= Show;
+            _world.TurnBaseModel.OnPlayerStepFinished -= Clear;
+        }
+
+        private void Show(IUnit unit)
+        {
+            if (unit is not UnitModel { IsDead: true } unitModel)
+            {
+                return;
+            }
+
+            Clear();
+
+            _currentInventory = new InventoryPresenter(unitModel.InventoryModel, _inventoryView, _viewDescriptions,
+                _uiContent, _world);
+
+            _currentInventory.Enable();
+        }
+
+        private void Clear()
+        {
+            _currentInventory?.Disable();
+            _currentInventory = null;
+        }
+    }
+}
