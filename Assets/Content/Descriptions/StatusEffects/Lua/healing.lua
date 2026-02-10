2c07cc36eafd6654d646182ed9b3b6d8781b869c
Valyutik <sergei.lind@tutamail.com>
2026-02-11 02:53:56 +0300

    add: healing status effect

---
+++ Assets/Content/Descriptions/StatusEffects/Lua/healing.lua
@@ -0,0 +1,19 @@
+﻿---@type StatusEffect
+local StatusEffect = {}
+
+---@param ctx StatusEffectContext
+function StatusEffect.CanApply(ctx)
+    return not ctx.unit.ActiveEffects:HasStatusEffect("curse")
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnTick(ctx)
+    local unit = ctx.unit
+    local effect = ctx.effect
+    local stacks = effect.stacks or 1
+    
+    local heal = 5 + stacks * 2
+    unit.Stats:Get("health"):ChangeValue(heal)
+end
+
+return StatusEffect
