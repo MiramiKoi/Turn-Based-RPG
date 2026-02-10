6828086718d13f57c453c366852a007ffd92afcd
Alexei <153764172+MiramiKoi@users.noreply.github.com>
2026-02-10 21:38:00 +0300

    add: loot model

---
+++ Assets/Runtime/UI/Loot/LootModel.cs
@@ -0,0 +1,17 @@
+using System;
+using Runtime.Common;
+using Runtime.Landscape.Grid.Cell;
+
+namespace Runtime.UI.Loot
+{
+    public class LootModel
+    {
+        public event Action<IUnit> OnLootRequested;
+        public CellModel PendingLootCell { get; set; }
+        
+        public void RequestLoot(IUnit unit)
+        {
+            OnLootRequested?.Invoke(unit);
+        }
+    }
+}
