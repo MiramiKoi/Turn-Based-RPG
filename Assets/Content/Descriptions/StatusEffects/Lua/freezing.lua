47acdd62c1afed2d3104950794d413f3f97aa66f
Valyutik <sergei.lind@tutamail.com>
2026-02-11 02:43:55 +0300

    add: freezing status effect

---
+++ Assets/Content/Descriptions/StatusEffects/Lua/freezing.lua
@@ -0,0 +1,37 @@
+﻿---@type StatusEffect
+local StatusEffect = {}
+
+---@param ctx StatusEffectContext
+function StatusEffect.CanApply(ctx)
+    local unit = ctx.unit
+    return not unit.ActiveEffects:HasStatusEffect("burn")
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.CanTick(ctx)
+    return StatusEffect.CanApply(ctx)
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnApply(ctx)
+    local unit = ctx.unit
+    unit:SetActionDisabled(1, true)
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnRemove(ctx)
+    local unit = ctx.unit
+    unit:SetActionDisabled(1, false)
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnTick(ctx)
+    local unit = ctx.unit
+    local effect = ctx.effect
+    local stacks = effect.stacks or 1
+
+    local damage = 1 + stacks
+    unit:TakeDamage(damage)
+end
+
+return StatusEffect
