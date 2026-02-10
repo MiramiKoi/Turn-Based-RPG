3c2af5b21f7ef775d6706bd57b6a0c272921b6cd
Maxim S. <its3zin.main@gmail.com>
2026-02-10 19:26:55 +0300

    add: inverse node

---
+++ Assets/Editor/Agents/Nodes/AgentInverseView.cs
@@ -0,0 +1,15 @@
+using Runtime.Descriptions.Agents.Nodes;
+
+namespace Editor.Agents.Nodes
+{
+    public class AgentInverseView : AgentBaseNodeView
+    {
+        public AgentInverseView(AgentNodeEditorWrapper data) : base(data)
+        {
+        }
+
+        public AgentInverseView(AgentNode data) : base(data)
+        {
+        }
+    }
+}
