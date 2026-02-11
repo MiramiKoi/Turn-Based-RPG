223dce05a73373df8c09f60ef2e29dcfa8899b01
Valyutik <sergei.lind@tutamail.com>
2026-02-11 04:00:54 +0300

    add: sleep status effect

---
+++ Assets/Content/Descriptions/StatusEffects/Lua/sleep.lua
@@ -0,0 +1,19 @@
+﻿---@type StatusEffect
+local StatusEffect = {}
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnApply(ctx)
+    local unit = ctx.unit
+    unit:SetActionDisabled(0, true)
+    unit.Stats:Get("visibility_radius"):Set(0)
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnRemove(ctx)
+    local unit = ctx.unit
+    unit:SetActionDisabled(0, false)
+    local visibility = unit.Stats:Get("visibility_radius")
+    visibility:Set(visibility.Description.MaxValue)
+end
+
+return StatusEffect
