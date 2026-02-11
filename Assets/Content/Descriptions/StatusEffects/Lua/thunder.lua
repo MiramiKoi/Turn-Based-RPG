531be739716fd7098e3c0d73ad5dd7d474b24769
Valyutik <sergei.lind@tutamail.com>
2026-02-11 04:16:02 +0300

    add: thunder status effect

---
+++ Assets/Content/Descriptions/StatusEffects/Lua/thunder.lua
@@ -0,0 +1,21 @@
+﻿---@type StatusEffect
+local StatusEffect = {}
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnApply(ctx)
+    local unit = ctx.unit
+    local effect = ctx.effect
+    local stacks = effect.stacks or 1
+
+    local damage = 5 + stacks * 3
+    unit:TakeDamage(damage)
+    unit:SetActionDisabled(0, true)
+end
+
+---@param ctx StatusEffectContext
+function StatusEffect.OnRemove(ctx)
+    local unit = ctx.unit
+    unit:SetActionDisabled(0, false)
+end
+
+return StatusEffect
