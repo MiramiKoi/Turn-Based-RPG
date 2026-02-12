745d1d6a2e4958b42a2c60b4d9ca6505a71d47c0
Valyutik <sergei.lind@tutamail.com>
2026-02-12 20:09:25 +0300

    add: player creation check

--- Assets/Runtime/Units/Collection/UnitModelCollection.cs
+++ Assets/Runtime/Units/Collection/UnitModelCollection.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Runtime.Descriptions;
 using Runtime.Extensions;
 using Runtime.ModelCollections;
+using Runtime.Player;
 using UnityEngine;
 
 namespace Runtime.Units.Collection
@@ -17,14 +18,14 @@ namespace Runtime.Units.Collection
 
         protected override UnitModel CreateModel(string descriptionKey)
         {
-            var model = new UnitModel(GetCurrentKey(), Vector2Int.one, _descriptions.UnitCollection[descriptionKey], _descriptions);
+            var model = descriptionKey == "character" ? new PlayerModel(GetCurrentKey(), Vector2Int.one, _descriptions.UnitCollection[descriptionKey], _descriptions) : new UnitModel(GetCurrentKey(), Vector2Int.one, _descriptions.UnitCollection[descriptionKey], _descriptions);
             
             return model;
         }
 
         protected override UnitModel CreateModelFromData(string id, Dictionary<string, object> data)
         {
-            var model = new UnitModel(id, data.GetVector2Int("position") ,_descriptions.UnitCollection[data.GetString("description_id")], _descriptions);
+            var model = id == "character" ? new PlayerModel(id, data.GetVector2Int("position") ,_descriptions.UnitCollection[data.GetString("description_id")], _descriptions) : new UnitModel(id, data.GetVector2Int("position") ,_descriptions.UnitCollection[data.GetString("description_id")], _descriptions);
 
             return model;
         }
