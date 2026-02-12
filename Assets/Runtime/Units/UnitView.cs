c68165e3c3645bd6b5fb76a482021eb03e3471ac
Valyutik <sergei.lind@tutamail.com>
2026-02-12 17:02:09 +0300

    refactor: cache the transform when awake

--- Assets/Runtime/Units/UnitView.cs
+++ Assets/Runtime/Units/UnitView.cs
@@ -5,12 +5,17 @@ namespace Runtime.Units
 {
     public class UnitView : MonoBehaviour
     {
-        [field: SerializeField] public Transform Transform { get; private set; }
+        public Transform Transform { get; private set; }
 
         [field: SerializeField] public SpriteRenderer SpriteRenderer { get; private set; }
 
         [field: SerializeField] public Animator Animator { get; private set; }
 
         [field: SerializeField] public StatusEffectCollectionView StatusEffectCollectionView { get; private set; }
+
+        private void Awake()
+        {
+            Transform = transform;
+        }
     }
 }
