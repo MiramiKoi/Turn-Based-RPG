ed55c80e71745f08c4d5bc2c9f18cea674a615a3
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-15 17:18:24 +0300

    add: trash handler

---
+++ Assets/Runtime/UI/Transfer/Handlers/TrashHandler.cs
@@ -0,0 +1,49 @@
+using Runtime.Core;
+using Runtime.Units;
+using UnityEngine;
+
+namespace Runtime.UI.Transfer.Handlers
+{
+    public class TrashHandler : BaseTransferHandler
+    {
+        private readonly World _world;
+
+        public TrashHandler(World world)
+        {
+            _world = world;
+        }
+
+        public override bool CanHandle(TransferModel context)
+        {
+            return context.TargetType == InventoryType.Trash;
+        }
+
+        public override void Handle(TransferModel context)
+        {
+            if (context.SourceCell?.ItemDescription == null)
+            {
+                return;
+            }
+
+            var item = context.SourceCell.ItemDescription;
+            var amount = context.SourceCell.Amount;
+
+            context.SourceCell.TryTake(amount);
+
+            var position = _world.PlayerModel.State.Position.Value;
+            var toPosition = position + Vector2Int.down;
+
+            var cellUnit = _world.GridModel.GetCell(toPosition).Unit;
+
+            if (cellUnit is UnitModel { Description: { Id: "loot" } } lootUnit)
+            {
+                lootUnit.Inventory.TryPutItem(item, amount);
+                return;
+            }
+
+            var unit = _world.UnitCollection.Create("loot");
+            unit.Inventory.TryPutItem(item, amount);
+            unit.Movement.SetPosition(toPosition);
+        }
+    }
+}
