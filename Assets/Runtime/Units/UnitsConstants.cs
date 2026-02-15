d3e0c738060f59f86362f6b54d6709c962360f97
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-15 17:21:57 +0300

    add: unit constants

---
+++ Assets/Runtime/Units/UnitsConstants.cs
@@ -0,0 +1,11 @@
+namespace Runtime.Units
+{
+    public static class UnitsConstants
+    {
+        public const string PlayerFraction = "player";
+        public const string TraderFraction = "trader";
+        public const string AnimalFraction = "animal";
+        public const string BossFraction = "boss";
+        public const string LootFraction = "loot";
+    }
+}
