9c33d4dc8b9b6deda46db5d483393c80c2b6ac37
Maxim S. <its3zin.main@gmail.com>
2026-02-16 16:13:40 +0300

    add: check stat percent

---
+++ Assets/Runtime/Descriptions/Agents/Commands/CheckStatPercentCommand.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using Runtime.Agents;
+using Runtime.Descriptions.Agents.Nodes;
+using Runtime.Extensions;
+using UnityEngine;
+
+namespace Runtime.Descriptions.Agents.Commands
+{
+    public class CheckStatPercentCommand : CommandDescription
+    {
+        private const string StatKey = "stat";
+        private const string OperationKey = "operation";
+        private const string ValueKey = "value";
+        
+        public override string Type => "check_stat_percent";
+
+        public string Stat { get; private set; } = string.Empty;
+        
+        public string Operation { get; private set; } = string.Empty;
+        
+        public float Value { get; private set; }
+        
+        public override NodeStatus Execute(IWorldContext context, IControllable controllable)
+        {
+            if (!controllable.Stats.Stats.TryGetValue(Stat, out var statModel))
+            {
+                return NodeStatus.Failure;
+            }
+            
+            var agentModel = controllable as AgentModel;
+            
+            var statValue = statModel.Value / agentModel.Description.Stats[Stat].MaxValue;
+
+            return Operation switch
+            {
+                "=" => Mathf.Approximately(statValue, Value) ? NodeStatus.Success : NodeStatus.Failure,
+                ">" => statValue > Value ? NodeStatus.Success : NodeStatus.Failure,
+                "<" => statValue < Value ? NodeStatus.Success : NodeStatus.Failure,
+                "<=" => statValue <= Value ? NodeStatus.Success : NodeStatus.Failure,
+                ">=" => statValue >= Value ? NodeStatus.Success : NodeStatus.Failure,
+                _ => NodeStatus.Failure
+            };
+        }
+
+        public override Dictionary<string, object> Serialize()
+        {
+            var dictionary =  base.Serialize();
+            
+            dictionary[StatKey] = Stat;
+            dictionary[OperationKey] = Operation;
+            dictionary[ValueKey] = Value;
+            
+            return dictionary;
+        }
+
+        public override void Deserialize(Dictionary<string, object> data)
+        {
+            Stat = data.GetString(StatKey);
+            Operation = data.GetString(OperationKey);
+            Value = data.GetFloat(ValueKey);
+        }
+    }
+}
